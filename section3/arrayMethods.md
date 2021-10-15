e6757e162f51e4b4ab1c3873e0c34a59ce67a32f
Madison Law <madisonlaw1@gmail.com>
2021-10-15 13:29:31 -0600

    Add Section3 - Arrays exercise

---
+++ section3/arrayMethods.md
@@ -0,0 +1,13 @@
+## Array Methods
+
+* Pop Method: Removes an item from the end of an array.
+...Example: `var last = fruits.pop()`..
+
+* Push Method: Adds an item to the end of the array.
+...Example: `var newLength = fruits.push('Orange')`..
+
+* Shift Method: Removes an item from the beginning of an array.
+...Example: `var first = fruits.shift()`..
+
+* Unshift Method: Adds an item to the beginning of an array.
+...Example: `var newLength = fruits.unshift('Strawberry')`..
--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -25,16 +25,22 @@ console.log(animals[0]);
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
 
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
 
+animals[2] = "Gorilla";
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
 
+var newLength = animals.push("Mongoose");
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
 
+var newLength = animals.unshift("Elephant");
+console.log(animals[0]);
 
 //-------------------
 // PART 2: Foods: Array Methods
@@ -42,24 +48,40 @@ console.log(animals[0]);
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
 
+var foods = ["Pancakes", "Waffles", "Bacon","Eggs"];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
 
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
 
+var newLength = foods.push("Broccoli");
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
 
+var last = foods.pop();
+console.log(foods);
 
-// YOU DO: Write code to add 3 new foods to the array. 
+
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
 
+var newLength = foods.push("Doughnuts");
+var newLength = foods.push("Oatmeal");
+var newLength = foods.push("Cereal");
+console.log(foods);
+
+
 // YOU DO: Remove the food that is in index position 0.
+
+var first = foods.shift();
+console.log(foods);
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,12 +101,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Instacart -> this app would store the shopping lists of users (both past and current) as an array likely.
+// 2: Spotify -> would store playlists and the contents of each playlist in array.
+// 3: Twitch -> would store the list of channels you follow in an array of some kind
 
 
 //-------------------
@@ -96,7 +118,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -107,6 +129,18 @@ var chargerType = "car";
 
 // Write your conditional here
 
+// This is setting up the If Statement and initial condition which is that the variable
+//percentBatteryLeft must be greater than or equal to 15 along with a logical operator (&&) that
+// states hasCharger must be true and chargerType must also equal "car" in order for the condition to evaluate as true.
+if (percentBatteryLeft >= 15 && hasCharger === true && chargerType === "car") {
+// This code will execute the below string and log it to the console if the above condition is true.
+  console.log("I can call an Uber.");
+// Now that we have the happy path we need to set up an else statement to tell the program what to do
+// if the above condition evaluates to false
+} else {
+// This code will execute the below string and log it to the console if the first condition is false.
+  console.log("I can't call an Uber.");
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
