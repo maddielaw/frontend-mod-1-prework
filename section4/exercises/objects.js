6368c32e40cb787d5f0072e6cc2297efb19cc882
Madison Law <madisonlaw1@gmail.com>
2021-10-18 11:53:36 -0600

    Add Objects.js to Section4 exercises

--- section4/exercises/objects.js
+++ section4/exercises/objects.js
@@ -17,30 +17,44 @@ var foods = {
 };
 
 console.log(foods);
-// Notice that the variable name is foods, plural, because it has the ability to hold info about 
-// potentially many foods. That plural name indicates to another developer that the variable stores 
+// Notice that the variable name is foods, plural, because it has the ability to hold info about
+// potentially many foods. That plural name indicates to another developer that the variable stores
 // either an Array or an Object.
 
 //-------------------
 // Part 1: Zoo Animals
 //-------------------
 
-// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and 
+// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and
 // the number of that type of animal at the zoo.
+
+var animals = {
+  giraffes: 6,
+  elephants: 2,
+  chimpanzees: 10
+};
 
 
 // Using the zoo that you created above, log the value of the first item in
 // the Object
 
+console.log(animals.giraffes);
+
 
 // Add an animal to the zoo Object, and log the updated Object.
 
+animals.zebra = 8
+console.log(animals);
 
 // Change the value for at least one of your animal keys, and log the updated Object.
 
+animals.elephants = 4
+console.log(animals);
 
 // Add another animal to the zoo Object, and log the updated Object.
 
+animals.mongoose = 15
+console.log(animals);
 
 //-------------------
 // Part 2: Email
@@ -56,6 +70,16 @@ value should be some appropriate value for that key. Work to have at least 5 key
 Log your email object to the console.
 */
 
+var emailAttributes = {
+  sender: "Maddie",
+  receiver: "Cameron",
+  subject: "Vacation ideas",
+  attachment: true,
+  timeStamp: "09:00 October 18, 2021"
+}
+
+console.log(emailAttributes);
+
 //-------------------
 // Part 3: Many Emails - OPTIONAL EXTENSION
 //-------------------
@@ -70,7 +94,7 @@ In the arrays exercise, we gave the following example of an array of Instagram p
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 /*
-Frankly, that was a very simplified version of the Array the Instagram developers have 
+Frankly, that was a very simplified version of the Array the Instagram developers have
 written and work with. Still probably slightly simplified as we don't know what their code
 actually looks like, but it may look more like this:
 */
@@ -82,7 +106,7 @@ var posts = [
     timeStamp: "4:37 PM August 13, 2019",
     numberLikes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -96,12 +120,39 @@ console.log(posts);
 console.log(posts[0]);
 
 /*
-The code snippet above shows an Array with 2 elements. Each element in an 
-Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+The code snippet above shows an Array with 2 elements. Each element in an
+Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 good to have some exposure before going into Mod 1.
 */
 
-// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 // key-value pairs you used in your email Object above.
 // Then, log the email Array to the console.
+
+
+var emailAttributes2 = [
+  {
+    sender: "Maddie",
+    receiver: "Cameron",
+    subject: "Vacation ideas",
+    attachment: true,
+    timeStamp: "09:00 October 18, 2021"
+  },
+  {
+    sender: "Osono",
+    receiver: "Kiki",
+    subject: "Tomorrow's delivery",
+    attachment: false,
+    timeStamp: "20:48 July 29, 1989"
+  },
+  {
+    sender: "Sophie",
+    receiver: "Howl",
+    subject: "Where we should move the castle next",
+    attachment: true,
+    timeStamp: "16:30 June 17, 2005"
+  }
+]
+
+console.log(emailAttributes2);
