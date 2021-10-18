2999898ace340fedff7b9a4d539cd00d950dd3dc
Madison Law <madisonlaw1@gmail.com>
2021-10-18 15:02:14 -0600

    Add classProperties.js to section4 exercises

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -17,9 +17,40 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+
+}
+
+var husky = new Dog();
+var poodle = new Dog();
+
+console.log(husky);
+console.log(poodle);
+
 // Prompt 2: Snack
 
+class Snack {
+
+}
+
+var celeryBoats = new Snack();
+var nachos = new Snack();
+
+console.log(celeryBoats);
+console.log(nachos);
+
 // Prompt 3: Shirt
+
+class SummerShirt {
+
+}
+
+var shortSleeve = new SummerShirt();
+var tankTop = new SummerShirt();
+
+console.log(shortSleeve);
+console.log(tankTop);
+
 
 //-------------------
 // Part 2: Properties
@@ -34,9 +65,40 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class DogBreed {
+  constructor() {
+    this.kidFriendly = true
+    this.gender = "female"
+    this.weight = 51
+  }
+}
+
+var husky = new DogBreed();
+console.log(husky);
+
 // Prompt 2: Snack
 
+class KidSnack {
+  constructor() {
+    this.healthy =  true
+    this.time = "afternoon"
+    this.ingredients = 3
+  }
+}
+ var celeryBoat = new KidSnack();
+ console.log(celeryBoat);
+
 // Prompt 3: Shirt
+
+class WorkShirt {
+  constructor() {
+    this.season = "fall"
+    this.size = "medium"
+    this.color = "burnt sienna"
+  }
+}
+var buttonUp = new WorkShirt();
+console.log(buttonUp);
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -52,6 +114,38 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class DogHealth {
+  constructor(weight, energy, temp) {
+    this.weight = weight;
+    this.energy = energy;
+    this.temperature = temp;
+  }
+}
+
+var husky = new DogHealth(142, "Low", 104);
+console.log(husky);
+
+
 // Prompt 2: Snack
 
+class SchoolSnack {
+  constructor(healthy, day, numIngredients) {
+    this.healthy = healthy;
+    this.day = day;
+    this.numIngredients = numIngredients;
+  }
+}
+var celeryBoat = new SchoolSnack(true, "Tuesday", 3);
+console.log(celeryBoat);
+
 // Prompt 3: Shirt
+
+class Shirt {
+  constructor(color, season, environment) {
+    this.color = color;
+    this.season = season;
+    this.environment = environment;
+  }
+}
+var blouse = new Shirt("Purple", "Winter", "Work Casual");
+console.log(blouse);
